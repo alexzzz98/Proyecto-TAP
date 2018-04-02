ff8900cf3f2c4581aff8ade715fc6fe518e3cf32
DiracEssence <alexpuntonuevesiete@gmail.com>
2018-04-02 01:42:38 -0700

    Subida de carpetas

---
+++ ProyectoTAP/Assets/Scripts/Movimientos/MovimientoASDW.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoASDW : MonoBehaviour {
+
+    public float velocidad;
+    public float sprint;
+
+    bool temp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            temp = true;
+            velocidad *= sprint;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.1f * velocidad);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -0.1f * velocidad);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f * velocidad, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f * velocidad, 0, 0);
+        }
+
+        if (temp)
+        {
+            velocidad /= sprint;
+            temp = false;
+        }
+    }
+}
---
+++ ProyectoTAP/Assets/Scripts/Movimientos/MovimientoCamara.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamara : MonoBehaviour {
+
+    public float velocidadCamara;
+    public Camera camara;
+
+    float h;
+    float v;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        h = Input.GetAxis("Mouse X") * velocidadCamara;
+        v = Input.GetAxis("Mouse Y") * velocidadCamara;
+
+        transform.Rotate(0, h, 0);
+        camara.transform.Rotate(-v, 0, 0);
+    }
+}
